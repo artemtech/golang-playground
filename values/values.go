13330c2a7b2e5fb57b27f9621422692661092a04
Sofyan Sugianto <sofyanartem@gmail.com>
2020-12-04 15:58:23 +0700

    belajar values

---
+++ values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("go" + "lang") // golang
+    fmt.Println("1+1 = ", 1+1) // 2
+    //9 = 1001
+    fmt.Println(9>>2) // shift ke kanan 2
+    fmt.Println(true && false) // false
+    fmt.Println(true || false) // true
+    fmt.Println(!true)  // false
+}
+
