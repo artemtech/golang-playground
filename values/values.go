2c4c57d2351fb75f8aa420322538dda8bb1dc131
Sofyan Sugianto <sofyanartem@gmail.com>
2020-12-04 16:56:01 +0700

    print length of character values

--- values/values.go
+++ values/values.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
     fmt.Println("go" + "lang") // golang
+    fmt.Println(len("golang")) // 6
     fmt.Println("1+1 = ", 1+1) // 2
     //9 = 1001
-    fmt.Println(9>>2) // shift ke kanan 2
+    fmt.Println(9>>2) // shift to right 2
     fmt.Println(true && false) // false
     fmt.Println(true || false) // true
     fmt.Println(!true)  // false
