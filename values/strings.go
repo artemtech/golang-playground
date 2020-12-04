a4fe57be728c1b5dd7e71d604e4c8b2d9f56ad7e
Sofyan Sugianto <sofyanartem@gmail.com>
2020-12-04 16:59:11 +0700

    playing with strings

---
+++ values/strings.go
@@ -0,0 +1,10 @@
+package main
+
+import "strings"
+
+func main(){
+        println(strings.Repeat("A",64))
+        println("asdf"+"wereere")
+        println(len("akhsfkjhaiuewhorikewru2983"))
+//        println(12+"Aasdf") // error
+}
