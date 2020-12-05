046deabe85dbe1cf3f3ca0ecccaf4e7a5c3ae270
Sofyan Sugianto <sofyanartem@gmail.com>
2020-12-05 13:33:16 +0700

    learn about variables

---
+++ variables/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+        var a = "initial value"
+        println(a) // initial value
+        var b, c int = 1, 2
+        println(b, c) // 1, 2
+        var d = true
+        println(d) // true
+        var e string
+        println(e)  // print empty line
+        var f int
+        println(f) // print 0
+
+        g := 1
+        println(g)
+        /*
+        := is a shorthand declaring and assigning 
+        variable in one line without var and data-type
+        */
+}
